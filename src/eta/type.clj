fb05386becb4be4bc2376dd8975a24094aa72977
Thomas Henderson <tom@mathpunk.net>
2018-02-27 15:14:46 -0600

    type: Specify freetext, timestamps (instant or date-only)

--- src/eta/type.clj
+++ src/eta/type.clj
@@ -1,10 +1,22 @@
 (ns eta.type
   (:require [clojure.string :as string]
-            [clojure.spec.alpha :as s]))
+            [clojure.spec.alpha :as s])
+  (:import org.joda.time.DateTime))
 
 ;; Tags and sets of them
 (s/def ::tag
   (s/and string?
          #(not (string/includes? % ","))))
 (s/def ::tag-set (s/coll-of ::tag))
+
+;; Freetext. Could be any string, but semantically, it's a little different. Wouldn't know how or whether to specify further.
+(s/def ::freetext string?)
+
+;; Timestamps. Instants should be okay, but sometimes I'll only have dates.
+(s/def ::timestamp (s/or :instant inst?
+                         :datetime #(= org.joda.time.DateTime (type %))
+                         :date #(= org.joda.time.LocalDate (type %))))
+
+;; "Links." What's the difference between an href, a uri, and a url?
+(s/def ::uri uri?)
 
--- test/eta/type_test.clj
+++ test/eta/type_test.clj
@@ -1,8 +1,9 @@
 (ns eta.type-test
-  (:require [eta.type :as sut]
+  (:require [eta.type]
+            [clj-time.core :as time]
             [clojure.spec.alpha :as s]
-            [clojure.test :refer [deftest testing is]]))
-
+            [clojure.test :refer [deftest testing is]])
+  (:import org.joda.time.LocalDate))
 
 (deftest test-tag-set
   (testing "tags are comma-separated, can have spaces"
@@ -12,6 +13,11 @@
   (testing "collections of tags"
     (is (s/valid? :eta.type/tag-set #{"term", "another term"}))
     (is (s/valid? :eta.type/tag-set ["term", "another term"]))
-    (is (not (s/valid? :eta.type/tag-set #{"comma, separated" "terms"})))))
+    (is (not (s/valid? :eta.type/tag-set #{"comma, separated" "terms"})))
+    (is (not (s/valid? :eta.type/tag-set #{"terms" 123})))))
+
+(deftest test-timestamps
+  (is (s/valid? :eta.type/timestamp (time/now)))
+  (is (s/valid? :eta.type/timestamp (time/local-date 2011 11 11))))
 
 
